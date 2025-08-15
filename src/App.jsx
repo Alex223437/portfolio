74419bc08be437287f185523105cf4d62790635c
Alex223437 <alex223437@gmail.com>
2025-08-15 11:38:34 +0200

    bug fixed + favicon

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.scss'
 import AnimatedRouter from './AnimatedRouter';
 import { initTheme } from './Components/Theme/theme';
 import { useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   useEffect(() => {
@@ -14,6 +15,22 @@ function App() {
     <HashRouter>
       <Layout>
         <AnimatedRouter />
+        <Toaster
+         position='top-left'
+         toastOptions={{
+          style: {
+            border: "2px solid transparent",
+            backgroundClip: "padding-box, border-box",
+            backgroundImage: `linear-gradient(rgba(30,41,59,0.6), rgba(30,41,59,0.6)), 
+                              linear-gradient(90deg, #D18B47 20%, #7C89C5 120%)`,
+            borderRadius: "12px",
+            backdropFilter: "blur(16px)",
+            color: "#fff",
+            padding: "14px 18px",
+            fontSize: "16px",
+            fontWeight: 500,
+          },
+        }}/>
       </Layout>
     </HashRouter>
   );
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,6 +5,7 @@ import ProjectImg from "../../assets/img/projects/project.png";
 import Cloud from "../../assets/img/bg-icons/cloud.svg";
 import { useEffect, useState, useRef } from "react";
 import { CSSTransition } from "react-transition-group";
+import toast, { Toaster } from "react-hot-toast";
 
 
 const projects = [
@@ -21,7 +22,7 @@ const projects = [
     title: "Personal Portfolio Website",
     description: "A modern, fully responsive personal portfolio website to showcase projects, skills, and contact information. Built with smooth animations, SEO optimization, fast performance, and an intuitive, user-friendly layout.",
     stack: ["React", "SCSS", "React Router", "Vite"],
-    liveDemo: "#",
+    liveDemo: "/",
     sourceCode: "https://github.com/Alex223437/portfolio"
   },
   {
@@ -51,7 +52,19 @@ const ProjectItem = ({image, title, description, stack, liveDemo, sourceCode }) 
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 18 18" fill="none">
               <path d="M10.2867 7.7133C9.60408 7.031 8.67846 6.64771 7.71334 6.64771C6.74821 6.64771 5.82259 7.031 5.14 7.7133L2.56584 10.2866C1.88324 10.9692 1.49976 11.895 1.49976 12.8604C1.49976 13.8257 1.88324 14.7515 2.56584 15.4341C3.24844 16.1167 4.17424 16.5002 5.13959 16.5002C6.10493 16.5002 7.03074 16.1167 7.71334 15.4341L9 14.1475M7.71332 10.2867C8.39591 10.969 9.32153 11.3523 10.2867 11.3523C11.2518 11.3523 12.1774 10.969 12.86 10.2867L15.4342 7.71334C16.1168 7.03074 16.5002 6.10493 16.5002 5.13959C16.5002 4.17424 16.1168 3.24844 15.4342 2.56584C14.7516 1.88324 13.8257 1.49976 12.8604 1.49976C11.8951 1.49976 10.9693 1.88324 10.2867 2.56584L8.99998 3.8525" stroke="currentColor" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
-            <a href={liveDemo} target="_blank" rel="noopener noreferrer">Live Demo</a>
+            <a
+              href={liveDemo}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={(e) => {
+                if (liveDemo === "#") {
+                  e.preventDefault();
+                  toast.error("Unfortunately live demo is not available :(");
+                }
+              }}
+            >
+              Live Demo
+            </a>
           </span>
           <span className="projects__item-link">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
@@ -64,7 +77,19 @@ const ProjectItem = ({image, title, description, stack, liveDemo, sourceCode }) 
                 </clipPath>
               </defs>
             </svg>
-            <a href={sourceCode} target="_blank" rel="noopener noreferrer">Source Code</a>
+            <a
+              href={sourceCode}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={(e) => {
+                if (sourceCode === "#") {
+                  e.preventDefault();
+                  toast("Source code is private :(", { icon: "🔒" });
+                }
+              }}
+            >
+              Source Code
+            </a>
           </span>
         </div>
       </div>
